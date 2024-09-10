5e53a2508f221e98aa71ea77e6c1bbe6b55f89aa
ChuckyCharles <157046505+ChuckyCharles@users.noreply.github.com>
2024-09-10 15:28:20 +0300

    Add files via upload

---
+++ backened/ec.tf
@@ -0,0 +1,29 @@
+# This is an ec2 instance that will host the backened server
+
+ # The instance is launched using AMI lookup
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+} 
+
